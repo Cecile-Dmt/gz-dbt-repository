e5cf34bd838852b08ea95b2965b23f15632fcb5b
ceciledomette@gmail.com <ceciledomette@gmail.com>
2025-02-12 15:09:38 +0000

    :

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,9 +1,9 @@
 -- calcuate margin 
 {% macro margin(revenue, purchase_cost) %} 
-    round({revenue} - {purchase_cost}) 
+    round({{revenue}} - {{purchase_cost}}) 
 {% endmacro %}
 
 -- calcuate margin percent
 {% macro margin_percent(margin, revenue) %}
-    round(SAFE_DIVIDE({{'margin'}}, {{'revenue'}}), 2)
+    round(SAFE_DIVIDE({{margin}}, {{revenue}}), 2)
 {% endmacro %}
