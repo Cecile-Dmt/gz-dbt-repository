45acd64bbef535d83e73e0b9e2a59d6d1e130595
ceciledomette@gmail.com <ceciledomette@gmail.com>
2025-02-12 14:58:15 +0000

    :

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -8,7 +8,7 @@
       purchase_price,
       ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
       ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
-      {{ margin_percent( {{ margin }}, {{ revenue }}) }} AS margin_percent
+      {{ margin_percent( {{ 'margin' }} : {{ 'revenue' }}) }} AS margin_percent
   FROM {{ref("stg_raw__sales")}} s
   LEFT JOIN {{ref("stg_raw__product")}} p
     ON s.pdt_id = p.products_id
